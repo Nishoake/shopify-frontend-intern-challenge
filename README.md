2bf984854c5d3cb50b7fd0df96338d326a8d82d4
Nishoake <nsribava@uwaterloo.ca>
2021-05-09 14:08:39 -0400

    Updated app.js server-side

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Shopify Front End Intern Challenge Fall 2021
-The Shoppies: Movie awards for entrepreneurs app
+The Shoppies App: Movie awards for entrepreneurs
 
 Challenge Description: https://docs.google.com/document/d/1SdR9rQpocsH5rPTOcxr9noqHRld5NJlylKO9Hf94U8U/edit#heading=h.6kafoyko4s5j
 
-Hosted Link:
+Hosted Link: https://theshoppiesapp.herokuapp.com/
--- server/app.js
+++ server/app.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const graphqlExpress = require('apollo-server-express')
-
-const myGraphQLSchema = // ... define or import your schema here!
 
 require('dotenv').config()
 
@@ -10,12 +7,11 @@ app = express()
 
 // Middleware
 app.use(bodyParser.json())
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }))
 
 // Intialize port
 const port = process.env.PORT || 3006
 app.listen(port, () => {
-  console.log(`Gym Share Prototype is currently listening at ${port}`)
+  console.log(`Gym Share app is currently listening at ${port}`)
 })
 
 // Routes
